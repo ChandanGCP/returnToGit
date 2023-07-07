041aa41a0de311421bb0527ca9c86d5d57b72f68
ChandanGCP <codingworld32@gmail.com>
2023-07-07 20:54:22 +0530

    student.java is modified

--- student.java
+++ student.java
@@ -2,4 +2,32 @@ public class student {
 public static void main(String args[]){
 System.out.println("Hello student");
 }
+}public class student {
+public static void main(String args[]){
+System.out.println("Hello student");
+}
+}
+
+public class student {
+public static void main(String args[]){
+System.out.println("Hello student");
+}
+}
+
+public class student {
+public static void main(String args[]){
+System.out.println("Hello student");
+}
+}
+
+public class student {
+public static void main(String args[]){
+System.out.println("Hello student");
+}
+}
+
+public class student {
+public static void main(String args[]){
+System.out.println("Hello student");
+}
 }
